9c3e47045a5fad7dfb3223ca130eaef35708c8d7
Nicholi Shiell <nicholishiell@gmail.com>
2024-04-25 11:11:31 -0400

    added example code

---
+++ example.py
@@ -0,0 +1,23 @@
+from ExtractedDocumentGenerator import ExtractedBoxGenerator
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def main():
+    
+    pdf_file_path = 'data/sample.pdf'
+    
+    doc_gen = ExtractedBoxGenerator()
+    
+    extract_doc = doc_gen.extract(pdf_file_path)
+
+    extract_doc.save_as_json('output.json')
+
+    for page in extract_doc:
+        print(page)
+        input('press ENTER for next page.')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__ == '__main__':
+    main()
